8be83b3d14f10c5605230f09feceb8fe055a09ce
bluefantasy728 <jiangguoxi728@hotmail.com>
2017-04-27 15:14:02 +0800

    Add files via upload

---
+++ src/js/page/index.js
@@ -0,0 +1,9 @@
+require("../../css/lib/reset.css");
+require("../../css/common/global.css");
+require("../../css/page/index.less");
+
+
+$('.g-bd').append('<p class="text">这个是Index页面</p>');
+$('.btn').on('click',function(){
+	alert(3);
+});
